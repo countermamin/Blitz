f137a0906d513536f20882b932c0410c20870ef0
Orzubek Maminov <104382267+countermamin@users.noreply.github.com>
2025-08-03 18:35:22 -0700

    Add user reply keyboard markup in common.py

--- core/scripts/telegrambot/utils/common.py
+++ core/scripts/telegrambot/utils/common.py
@@ -5,4 +5,24 @@ def create_main_markup():
     markup.row('Add User', 'Show User')
     markup.row('Delete User', 'Server Info')
     markup.row('Backup Server')
+
+    
+
+def create_user_markup():
+    """
+    Create a reply keyboard for normal users.
+
+    Buttons:
+    - Subscribe: start a subscription process
+    - My Link: show current subscription link
+    - Revoke Link: revoke and regenerate link
+    """
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    # Row for subscription option
+    markup.row('Subscribe')
+    # Row for retrieving or revoking user link
+    markup.row('My Link', 'Revoke Link')
     return markup
+
+    return markup
+
